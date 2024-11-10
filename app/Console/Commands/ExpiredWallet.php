215a89533022175a6f504e25d2bc0c2ab9fe90a0
mohamed khairy <mohamed.khairy.eg@gmail.com>
2024-11-10 09:33:49 -1200

    Update ExpiredWallet.php

--- app/Console/Commands/ExpiredWallet.php
+++ app/Console/Commands/ExpiredWallet.php
@@ -45,9 +45,9 @@ class ExpiredWallet extends Command
 
 
             $logs = WalletLog::with('customer')
-                ->whereDate('expired_at', date('Y-m-d'))->get();
+                ->whereDate('expired_at', date('Y-m-d', strtotime('-1 day')))->get();
 
-                info($logs);
+                
             foreach ($logs as $key => $log) {
                 $amount = $log->new_amount - $log->last_amount;
                 if (
@@ -58,33 +58,37 @@ class ExpiredWallet extends Command
                     ->count() < 1
                 ) {
 
+                    
                     $remove = WalletLog::where([
                         'action_id' => $log->action_id,
                         'customer_id' => $log->customer_id,
-                        'action' => 'remove',
+                        'action' => 'expiry',
                     ])->first();
 
                     if (!$remove) {
 
                         $new_amount = $log->customer->wallet - $amount;
 
-                        $log = WalletLog::create([
-                            'user_id' => null,
-                            'customer_id' => $log->customer_id,
-                            'last_amount' => $log->customer->wallet,
-                            'new_amount' => $new_amount,
-                            'action_id' =>  $log->action_id,
-                            'action' => 'expiry',
-                            'expired_days' => null,
-                            'expired_at' => null,
-                            'message_ar' => ' تسوية رصيد منتهي الصلاحية',
-                            'message_en' => 'Expired balance settlement '
-                        ]);
-
-                        $log->customer->update(['wallet' => $new_amount]);
+                        if($new_amount > 0){
+                            $log = WalletLog::create([
+                                'user_id' => null,
+                                'customer_id' => $log->customer_id,
+                                'last_amount' => $log->customer->wallet,
+                                'new_amount' => $new_amount > 0 ? $new_amount : 0,
+                                'action_id' =>  $log->action_id,
+                                'action' => 'expiry',
+                                'expired_days' => null,
+                                'expired_at' => null,
+                                'message_ar' => ' تسوية رصيد منتهي الصلاحية',
+                                'message_en' => 'Expired balance settlement '
+                            ]);
+    
+                            $log->customer->update(['wallet' => $new_amount]);
+                        }
                     }
                 }
-            }
+            }   
+            
 
             DB::commit();
             //code...
