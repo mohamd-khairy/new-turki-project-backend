8f2e9b1ab8e95e95672104090753b40011a23b1d
Mohamed Khairy <mohamed.khairy.eg@gmail.com>
2024-12-12 10:40:27 +0300

    fix

--- app/Http/Controllers/API/Cashier/CashierController.php
+++ app/Http/Controllers/API/Cashier/CashierController.php
@@ -515,8 +515,8 @@ class CashierController extends Controller
 
     private function getAuthCountryCode()
     {
-        return null;
-        // return auth()->user()->mobile_country_code === '+966' ? 1 : 4;
+        // return null;
+        return auth()->user()->mobile_country_code === '+966' ? 1 : 4;
     }
 
     private function getCountryCode($customer)
