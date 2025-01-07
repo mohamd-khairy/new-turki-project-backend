12ee3c827d0771f5d67c71b252aaa5b055222ac9
Mohamed Khairy <mohamed.khairy.eg@gmail.com>
2025-01-07 23:03:38 +0300

    fix

--- app/Models/Discount.php
+++ app/Models/Discount.php
@@ -262,19 +262,19 @@ class Discount extends Model
         }
 
         $validSizeIds = [];
-        // if ($coupon->is_by_size) {
-        if ($coupon->size_ids != null) {
-            $validSizeIds = is_array($coupon->size_ids) ? $coupon->size_ids : explode(',', trim($coupon->size_ids));
-            // products in cart
-            foreach ($validSizeIds as $sizeId) {
-                if (!in_array($sizeId, $validSizeIds)) {
-                    return [8, "coupon is not valid for some sizes"];
-                } else {
-                    $entryCount = $entryCount + 1;
+        if ($coupon->is_by_size) {
+            if ($coupon->size_ids != null) {
+                $validSizeIds = is_array($coupon->size_ids) ? $coupon->size_ids : explode(',', trim($coupon->size_ids));
+                // products in cart
+                foreach ($validSizeIds as $sizeId) {
+                    if (!in_array($sizeId, $validSizeIds)) {
+                        return [8, "coupon is not valid for some sizes"];
+                    } else {
+                        $entryCount = $entryCount + 1;
+                    }
                 }
             }
         }
-        // }
 
 
         $notApplicableProducts = [];
