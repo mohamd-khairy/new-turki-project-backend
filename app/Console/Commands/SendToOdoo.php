dd32cdbbc5bd5017b95bb7e228c4857fb2032aef
Mohamed Khairy <mohamed.khairy.eg@gmail.com>
2025-03-19 10:36:56 +0300

    fix

--- app/Console/Commands/SendToOdoo.php
+++ app/Console/Commands/SendToOdoo.php
@@ -56,7 +56,7 @@ class SendToOdoo extends Command
                 // })
                 ->where('ref_no', 'like', 'SA%')
                 // ->whereDate('created_at', date('Y-m-d'))
-                ->whereNotIn('order_state_id' , ['104' , '200'])
+                ->whereNotIn('order_state_id', ['104', '109', '200'])
                 ->where('created_at', '<=', date('Y-m-d H:i:s', strtotime('-4 minutes')))
                 ->orderBy('id', 'desc')
                 ->take(10)
